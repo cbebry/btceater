8172fee2dafac7394027cf0bb333093081a7d88c
Christopher Bebry <invalidsyntax@gmail.com>
2014-05-04 19:01:19 -0400

    The beginning

---
+++ index.js
@@ -0,0 +1,48 @@
+var Hapi = require('hapi');
+
+// Create a server with a host and port
+var server = Hapi.createServer('0.0.0.0', 7777);
+
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function (request, reply) {
+
+        reply('hello world');
+    }
+});
+server.route({
+	method: "GET",
+	path: "/",
+	config: {
+		handler: function (req, reply) {
+			var Request = require('request');
+			Request('http://cdn.spectrumbranch.com/server/markets.php', function(err, response, body) {
+				if (!err && response.statusCode == 200) {
+					var data = JSON.parse(body);
+					var organizer = require('./lib/organizer');
+					reply(organizer.process(data.return));
+				}
+			});
+		}
+	}
+});
+/*
+server.route({
+	method: "GET",
+	path: "/{path*}",
+	config: {
+		handler: {
+			directory: {
+				path: "./static/",
+				listing: false,
+				redirectToSlash:true
+			}
+		},
+		auth: false
+	}
+});
+*/
+// Start the server
+server.start();
+console.log('up');
